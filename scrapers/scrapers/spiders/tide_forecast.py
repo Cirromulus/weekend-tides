c250b6d3cb0291e2f014c329ab746a7826b30219
Pascal Pieper <accounts@pascalpieper.de>
2022-11-07 11:26:21 +0100

    initial commit.

---
+++ scrapers/scrapers/spiders/tide_forecast.py
@@ -0,0 +1,37 @@
+import scrapy
+import datetime
+
+class TideForecastSpider(scrapy.Spider):
+    name = 'tide-forecast_extractor'
+    start_urls = ['https://www.tide-forecast.com/locations/Robbensudsteert-Germany/tides/latest']
+    
+    def parse(self, response):
+        daycards = response.css('div.tide-day')
+        for daycard in daycards:
+            datetext = daycard.css('.tide-day__date::text').get().split(": ")[-1]
+            datetext_format = '%A %d %B %Y'
+            thisday_date = datetime.datetime.strptime(datetext, datetext_format)
+            
+            thisday_tide_points = list()
+            rows = daycard.css('tr')
+            for row in rows:
+                elems = daycard.css('td')
+                for i, elem in enumerate(elems):
+                    text = elem.css('::text').get()
+                    if (text == 'Low Tide' or text == 'High Tide'):
+                        timetext = elems[i+1].css('::text').get()
+                        # fuck america
+                        if (timetext.split(":")[0] == "00"):
+                            timetext = "12:" + timetext.split(":")[1]
+                        height = elems[i+2].css('::text').get()
+                        full_time = datetime.datetime.strptime(datetext + " " + timetext, datetext_format + ' %I:%M %p')
+                        print ("Found " + text + " at " + str(full_time) + " (" + height + ")")
+                        thisday_tide_points.append({'Tide Point': text, 'time' : full_time, 'height_text' : height})
+            
+            sunmooncells = daycard.css('td.tide-day__sun-moon-cell')
+            sunrise_text = sunmooncells[0].css('.tide-day__value::text').get()
+            sunset_text = sunmooncells[1].css('.tide-day__value::text').get()
+            sunrise_date = datetime.datetime.strptime(datetext + " " + sunrise_text, datetext_format + ' %I:%M%p')
+            sunset_date = datetime.datetime.strptime(datetext + " " + sunset_text, datetext_format + ' %I:%M%p')
+            
+            yield {'day' : thisday_date, 'tides' : thisday_tide_points, 'sunrise' : sunrise_date, 'sunset' : sunset_date}
